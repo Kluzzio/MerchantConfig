a1a6280e72efa790fe9bc49d2a1371c10d067c32
Kluzzio <kluzzio.amp@gmail.com>
2022-09-17 21:12:57 -0700

    Fix Packages now that on GitHub

--- src/main/java/kluzzio/merchantconfig/VillagerEntityMixin.java
+++ src/main/java/kluzzio/merchantconfig/VillagerEntityMixin.java
@@ -1,10 +1,15 @@
-package com.github.kluzzio.lootconfig.mixins;
+package kluzzio.merchantconfig;
 
+import net.minecraft.entity.passive.VillagerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(VillagerEntityMixin.class)
+@Mixin(VillagerEntity.class)
 public class VillagerEntityMixin {
 
-    @Inject(method = "fillRecipes")
+    @Inject(method = "fillRecipes", at = @At("RETURN"))
+    public void ahhhhh(CallbackInfo ci) {
+    }
 }
